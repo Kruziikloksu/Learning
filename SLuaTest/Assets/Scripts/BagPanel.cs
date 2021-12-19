4de32da78ff9a305de3133d3f7c59fad3e21a8ab
Kruziikloksu <10025501+Kruziikloksu@user.noreply.gitee.com>
2021-12-19 16:28:33 +0800

    no commit message

--- SLuaTest/Assets/Scripts/BagPanel.cs
+++ SLuaTest/Assets/Scripts/BagPanel.cs
@@ -15,13 +15,11 @@ public class BagPanel : MonoBehaviour
     public ItemSO thisItem;
     public BagSO myBag;
 
-
-    //public Button showBagButton;
     public string luaFileName = "BagPanel";
 
 
-    LuaFunction updateItemInfo;
-    LuaFunction closeBagPanel;
+    public LuaFunction updateItemInfo;
+    public LuaFunction closeBagPanel;
     public LuaFunction createNewItem;
     public LuaFunction refreshItems;
     public LuaFunction dropTheseItem;
@@ -40,8 +38,6 @@ public class BagPanel : MonoBehaviour
         LuaResourse thisLuaRes = AssetBundleManager.LoadResource<LuaResourse>(strFile, "myluares");
         return System.Text.Encoding.Default.GetBytes(thisLuaRes.LuaString);
     }
-
-
     void Awake()
     {
         if (instance != null)
@@ -49,91 +45,47 @@ public class BagPanel : MonoBehaviour
             Destroy(this);
         }
         instance = this;
-
     }
 
     private void OnEnable()
     {
+        if (instance != null)
+        {
+            Destroy(this);
+        }
+        instance = this;
+
         myBag = AssetBundleManager.LoadResource<BagSO>("Bag", "bagpanel");
 
-        //instance.itemInfo.text = "";//文本重置
-        
-        luaSvr = new LuaSvr();// 初始化LuaSvr LuaSvr是对LuaState的一个封装
+        luaSvr = new LuaSvr();// 初始化LuaSvr
         LuaSvr.MainState luaMainState = LuaSvr.mainState;
-        // 如果不用init方法初始化,在Lua中不能import
+        //用init方法初始化
         luaSvr.init(null, () =>
         {
-            luaMainState.loaderDelegate += LuaReourcesFileLoader;//在mainState的委托loaderDelegate里注册方法
+            luaMainState.loaderDelegate += LuaReourcesFileLoader;
             self = (LuaTable)luaSvr.start(luaFileName);
             closeBagPanel = luaMainState.getFunction("CloseBagPanel");
             createNewItem = luaMainState.getFunction("CreateNewItem");
             refreshItems = luaMainState.getFunction("RefreshItems");
             dropTheseItem = luaMainState.getFunction("DropTheseItem");
         });
-        //thisItem = myBag.itemInBag[3];
         RefreshItems();
-        //CreateNewItem(thisItem);
-
     }
 
     public static void UpdateItemInfo(string itemInfo)
     {
-        //updateItemInfo.call(itemInfo);
         instance.itemInfo.text = itemInfo.ToString();
     }
-    /*
-    //public void AddNewItem()
-    //{
-    //    if (!myBag.itemInBag.Contains(thisItem))
-    //    {
-    //        myBag.itemInBag.Add(thisItem);
-    //        CreateNewItem(thisItem);
-    //    }
-    //    RefreshItems();
-    //}
-    */
     public void CreateNewItem(ItemSO itemDataSO)
     {
-
         createNewItem.call(itemDataSO);
     }
-    /*
-if (itemDataSO.itemHeldNum > 0)
-{
-    ItemSlot newItem = Instantiate(instance.itemSlot, instance.slotGrid.transform.position, Quaternion.identity);
-    newItem.gameObject.transform.SetParent(instance.slotGrid.transform);
-    newItem.thisItem = itemDataSO;
-    newItem.itemName.text = itemDataSO.itemName.ToString();
-    newItem.itemHeldNum.text = itemDataSO.itemHeldNum.ToString();
-    newItem.itemInfo = itemDataSO.itemDescription;
-}
-*/
-    public void RefreshItems()  //刷新Action
+    public void RefreshItems() 
     {
-
         refreshItems.call();
     }
-    /*
-    //for (int i = 0; i < instance.slotGrid.transform.childCount; i++)
-    //{
-    //    if (instance.slotGrid.transform.childCount == 0)
-    //    {
-    //        break;
-    //    }
-    //    Destroy(instance.slotGrid.transform.GetChild(i).gameObject);
-    //}
-    //for (int i = 0; i < instance.myBag.itemInBag.Count; i++)
-    ////foreach(ItemSO itemSO in myBag.itemInBag)
-    //{
-    //    thisItem = myBag.itemInBag[i]; ;
-    //    //CreateNewItem(myBag.itemInBag[i]);
-    //    CreateNewItem(thisItem);
-    //}
-    */
     public void DropTheseItem()
     {
-        //myBag.itemInBag.Remove(thisItem);
-        //RefreshItems();
         dropTheseItem.call();
     }
     public void CloseBagPanel()
